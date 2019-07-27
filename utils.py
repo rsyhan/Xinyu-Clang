13db2f7b1237f43c0bbdfd61f4e9c51e7633f80b
Nathan Chancellor <natechancellor@gmail.com>
2019-07-26 19:24:36 -0700

    tc-build: Use master branch for binutils
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>
    Signed-off-by: HanyanI <yuhan@rsyhan.me>

--- utils.py
+++ utils.py
@@ -17,61 +17,26 @@ def create_gitignore(folder):
         gitignore.write("*")
 
 
-def current_binutils():
+def fetch_binutils(folder, update=True):
     """
-    Simple getter for current stable binutils release
-    :return: The current stable release of binutils
-    """
-    return "binutils-2.34"
-
-
-def download_binutils(folder):
-    """
-    Downloads the latest stable version of binutils
+    Clones/updates the binutils repo
     :param folder: Directory to download binutils to
     """
-    binutils = current_binutils()
-    binutils_folder = folder.joinpath(binutils)
-    if not binutils_folder.is_dir():
-        # Remove any previous copies of binutils
-        for entity in folder.glob('binutils-*'):
-            if entity.is_dir():
-                shutil.rmtree(entity.as_posix())
-            else:
-                entity.unlink()
-
-        # Download the tarball
-        binutils_tarball = folder.joinpath(binutils + ".tar.xz")
+    binutils_folder = folder.joinpath("binutils")
+    if binutils_folder.is_dir():
+        if update:
+            print_header("Updating binutils")
+            subprocess.run(
+                ["git", "-C",
+                 binutils_folder.as_posix(), "pull", "--rebase"],
+                check=True)
+    else:
+        print_header("Downloading binutils")
         subprocess.run([
-            "curl", "-LSs", "-o",
-            binutils_tarball.as_posix(),
-            "https://ftp.gnu.org/gnu/binutils/" + binutils_tarball.name
+            "git", "clone", "git://sourceware.org/git/binutils-gdb.git",
+            binutils_folder.as_posix()
         ],
                        check=True)
-        verify_binutils_checksum(binutils_tarball)
-        # Extract the tarball then remove it
-        subprocess.run(["tar", "-xJf", binutils_tarball.name],
-                       check=True,
-                       cwd=folder.as_posix())
-        create_gitignore(binutils_folder)
-        binutils_tarball.unlink()
-
-
-def verify_binutils_checksum(file):
-    # Check the sha256sum of the downloaded package with a known good one
-    # To regenerate the sha256sum, download the .tar.xz and .tar.xz.sig files
-    # $ gpg --verify *.tar.xz.sig *.tar.xz
-    # $ sha256sum *.tar.xz
-    file_hash = hashlib.sha256()
-    with file.open("rb") as f:
-        while True:
-            data = f.read(65536)
-            if not data:
-                break
-            file_hash.update(data)
-    good_hash = "f00b0e8803dc9bab1e2165bd568528135be734df3fabf8d0161828cd56028952"
-    if file_hash.hexdigest() != good_hash:
-        raise RuntimeError("binutils sha256sum does not match known good one!")
 
 
 def print_header(string):
